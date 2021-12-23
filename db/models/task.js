b730a1eb4fc6bcb9de546aed58062de91d0a1e11
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-23 14:17:14 +0100

    [ bug-fix ] mongodb timestamps

--- db/models/task.js
+++ db/models/task.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 
-const Task = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const Task = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Task", Task);
--- db/models/user.js
+++ db/models/user.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 
-const User = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const User = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    tasks: {
+      type: [String],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  tasks: {
-    type: [String],
-  },
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", User);
