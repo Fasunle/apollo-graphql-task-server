31bead5383493d45e4fb33b38c349dddbeae7d39
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-24 17:13:16 +0100

    [Add] jsonwebtoken to generate session token

--- utils/variables.js
+++ utils/variables.js
@@ -4,7 +4,6 @@ const { MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_NAME, SERVER_PORT } =
 /**
  * MONGODB_URI construct the connection string from the parameters
  */
-exports.MONGODB_URI = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@sandbox.qnvqb.mongodb.net/${MONGODB_NAME}?retryWrites=true &
-w=majority`;
+exports.MONGODB_URI = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@sandbox.qnvqb.mongodb.net/${MONGODB_NAME}`;
 
 exports.SERVER_PORT = SERVER_PORT || 5000;
