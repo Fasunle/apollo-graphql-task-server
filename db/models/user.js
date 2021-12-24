223d02cd0338e58aacd695a9e2ddc91572397755
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-24 16:57:55 +0100

    [MONGO USER MODEL] password field was missing

--- db/models/user.js
+++ db/models/user.js
@@ -10,6 +10,10 @@ const User = new mongoose.Schema(
       type: String,
       required: true,
     },
+    password: {
+      type: String,
+      required: true,
+    },
     tasks: {
       type: [String],
     },
