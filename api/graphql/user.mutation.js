d57ccf02ee367a18a1793baaf3a20cd054c0c3c4
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-24 17:42:47 +0100

    [Functional] generate token for session management

--- api/graphql/user.mutation.js
+++ api/graphql/user.mutation.js
@@ -1,11 +1,13 @@
 const bcrypt = require("bcrypt");
 const { validateUser, validateEmail } = require("./validators");
 const User = require("../../db/models/user");
+const { generateToken } = require("./utils");
 
 const signUpUser = async (parent, { user }) => {
   // validate user input
   const { valid } = validateUser(user);
   // sign up the user
+  const userPayload = { email: user.email, name: user.name };
   try {
     const found = await User.findOne({ email: user.email });
     if (found) {
@@ -13,6 +15,8 @@ const signUpUser = async (parent, { user }) => {
     }
     user.password = await bcrypt.hash(user.password, 10);
     const newUser = await User(user).save();
+    // add token to the newUser Object
+    newUser.token = generateToken(userPayload, user.password);
     return newUser;
   } catch (error) {
     console.error("Error occured ", error);
@@ -24,11 +28,16 @@ const loginUser = async (parent, { email, password }) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      // const newUser = { ...user, token: "" };
+      const userData = {
+        name: user.name,
+        email: user.email,
+      };
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
         throw new UserInputError("Email or password is wrong");
       }
+      // add token to the user Object
+      user.token = generateToken(userData, password);
       return user;
     }
     throw new UserInputError("User does not exist! try to signup");
