faf7463ae47a5905c75e217dfe0f7ed244338b4e
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-24 17:41:40 +0100

    [Bug-fix] remove ./utils import

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -2,7 +2,6 @@ const taskQueries = require("./task.query");
 const userQueries = require("./user.query");
 const userMutations = require("./user.mutation");
 const taskMutations = require("./task.mutation");
-const { mutations } = require("./utils");
 
 // resolver object with Query and Mutation fields
 module.exports = {
@@ -13,6 +12,5 @@ module.exports = {
   Mutation: {
     ...taskMutations,
     ...userMutations,
-    ...mutations,
   },
 };
