9101d2dc4c49ff257a36f5157e6bf61c670b1092
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-25 21:09:31 +0100

    create task if logged in

--- api/graphql/task.mutation.js
+++ api/graphql/task.mutation.js
@@ -1,6 +1,16 @@
-const createTask = (parent, { task }) => {
-  console.log({ task });
-  return { ...task, id: "2" };
-};
+const { combineResolvers } = require("graphql-resolvers");
+const Task = require("../../db/models/task");
+const User = require("../../db/models/user");
+const { isAuthenticated } = require("../../middleware");
+
+const createTask = combineResolvers(
+  isAuthenticated,
+  async (parent, { task }, { email }) => {
+    const user = await User.findOne({ email });
+    const newTask = await Task({ ...task, id: user.id });
+    await newTask.save();
+    return newTask;
+  }
+);
 
 module.exports = { createTask };
