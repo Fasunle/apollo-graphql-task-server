2d282c6cc3fafd69bb71e2bc1be6c8dc7f12d3c3
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-28 10:48:21 +0100

    [Bugfix] handle user is null

--- api/graphql/task.mutation.js
+++ api/graphql/task.mutation.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require("apollo-server");
 const { combineResolvers } = require("graphql-resolvers");
 const Task = require("../../db/models/task");
 const User = require("../../db/models/user");
@@ -6,11 +7,22 @@ const { isAuthenticated } = require("../../middleware");
 const createTask = combineResolvers(
   isAuthenticated,
   async (parent, { task }, { email }) => {
-    const user = await User.findOne({ email });
-    const newTask = await Task({ ...task, id: user.id });
-    await newTask.save();
-    return newTask;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error("Please signup and come back to create tasks");
+      }
+      const newTask = await Task({ ...task, id: user.id });
+      await newTask.save();
+      return newTask;
+    } catch (error) {
+      throw new AuthenticationError(error.message);
+    }
   }
 );
+
+const deleteTask = (id) => {
+  return id;
+};
 
 module.exports = { createTask };
