f2295010eedbe7d382a6f603d1a3332f22a43065
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-28 10:26:08 +0100

    [Bugfix] token expire in 1 day from login

--- api/graphql/utils.js
+++ api/graphql/utils.js
@@ -1,6 +1,12 @@
 const jsonwebtoken = require("jsonwebtoken");
 
 exports.generateToken = async (payload, secret) => {
-  const token = await jsonwebtoken.sign(payload, secret);
-  return token;
+  try {
+    const token = await jsonwebtoken.sign(payload, secret, {
+      expiresIn: "1d", // this allows the token to expire and request the user to login again
+    });
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
 };
