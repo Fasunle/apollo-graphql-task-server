b8a487b46e3e5ff896ff6ff7d0c3705e27c4f21c
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-29 08:52:32 +0100

    [Bugfix] Authentication middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,6 @@
 const { skip } = require("graphql-resolvers");
+const { verify } = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server");
 
 /**
  *
@@ -7,8 +9,31 @@ const { skip } = require("graphql-resolvers");
  * @param {*} param2
  * throw error if the user is not logged in
  */
-exports.isAuthenticated = (parent, _, { email }) => {
-  if (!email) {
+exports.isAuthenticated = (parent, _, { req }) => {
+  const secret = process.env.TOKEN_SECRET || "you are a 419";
+
+  // token passed in the header.authorization
+  const authHeader = req.headers;
+
+  if (!authHeader.authorization) {
+    throw new Error("Authorization header must be provided!");
+  }
+
+  const token = authHeader.authorization.split(" ")[1];
+
+  try {
+    // throw automatically if invalid token
+    const user = verify(token, secret);
+    if (user) {
+      req.email = user.email;
+    }
+  } catch (error) {
+    throw new AuthenticationError(
+      error.message + " please login with a valid token"
+    );
+  }
+
+  if (!req.email) {
     throw new Error("Access denied! Please login to continue");
   }
   //not a function
