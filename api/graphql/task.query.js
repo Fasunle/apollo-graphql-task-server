ff8140b6721dfef2eed01bda010772df11485bfc
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-29 12:35:47 +0100

    [Update] user can get a task or tasks

--- api/graphql/task.query.js
+++ api/graphql/task.query.js
@@ -1,34 +1,43 @@
-const getTask = async (parent, { taskId }) => {
-  console.log({ taskId });
-  return {
-    id: 1,
-    title: "Task 1",
-    description: "Go to market",
-  };
-};
-const getTasks = async () => {
-  return [
-    {
-      id: 1,
-      name: "Task 1",
-      description: "Go to market",
-    },
-    {
-      id: 2,
-      name: "Task 1",
-      description: "Go to market",
-    },
-    {
-      id: 3,
-      name: "Task 1",
-      description: "Go to market",
-    },
-    {
-      id: 4,
-      name: "Task 1",
-      description: "Go to market",
-    },
-  ];
-};
+const Task = require("../../db/models/task");
+
+const { combineResolvers } = require("graphql-resolvers");
+const { isAuthenticated } = require("../../middleware");
+const User = require("../../db/models/user");
+
+const getTask = combineResolvers(
+  isAuthenticated,
+  async (parent, _, { req }) => {
+    const email = req.email;
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new Error("user does not exist!");
+      }
+      const task = await Task.findOne({ createdBy: user.id });
+      return task;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+);
+
+const getTasks = combineResolvers(
+  isAuthenticated,
+  async (parent, _, { req }) => {
+    const email = req.email;
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new Error("user does not exist!");
+      }
+      const task = await Task.find({ createdBy: user.id });
+      return task;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+);
 
 module.exports = { getTask, getTasks };
